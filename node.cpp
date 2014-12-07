ee2905516fd97a9dddaaba0955d51db3bc980107
dyunkim <dykim@smu.edu>
2014-12-07 02:39:52 -0600

    updated Node classes

--- node.cpp
+++ node.cpp
@@ -1,5 +1,77 @@
 #include "node.h"
 
-//Node::Node()
-//{
-//}
+Node::Node()
+{
+
+}
+
+Node::Node(string &word)
+{
+    this->word = word;
+}
+
+Node::Node(string& word, int id)
+{
+    this->word = word;
+    this->ids.emplace(id, FrequencyNode(id));
+}
+
+Node::Node(string& word, int id, int frequency)
+{
+    this->word = word;
+    this->ids.emplace(id, FrequencyNode(id, frequency));
+}
+
+Node::~Node()
+{
+
+}
+
+unordered_map<int, FrequencyNode> &Node::getIds()
+{
+    return ids;
+}
+
+string &Node::getWord()
+{
+    return word;
+}
+
+void Node::setWord(string word)
+{
+    this->word = word;
+}
+
+void Node::insertId(int id)
+{
+    if(exists(id))
+    {
+        unordered_map <int, FrequencyNode>::iterator got = ids.find(id);
+        if (got == ids.end())
+        {
+            cout << "not found" << endl;
+        }
+        else
+        {
+            got->second.incFrequency();
+            //cout << got->second << " ";
+        }
+
+    }
+    else
+        ids.emplace(id, FrequencyNode(id));
+}
+
+void Node::insertId(int id, int frequency)
+{
+        ids.emplace(id, FrequencyNode(id, frequency));
+}
+
+bool Node::exists(int id)
+{
+    auto search = ids.find(id);
+    if (search != ids.end())
+        return true;
+    else
+        return false;
+}
--- node.h
+++ node.h
@@ -1,64 +1,40 @@
 #ifndef NODE_H
 #define NODE_H
+#include "LinkedList.h"
+#include "frequencynode.h"
 #include <vector>
 #include <unordered_map>
+
 
 using namespace std;
 class Node
 {
-private:
+private: // Member Variables
     string word;
-    unordered_map<int, int> ids;
-public:
-    Node(){}
-    Node(string &word)
+    unordered_map<int, FrequencyNode> ids;
+public: // Constructor || Destructor
+    Node();
+    Node(string &word);
+    Node(string& word, int id);
+    Node(string& word, int id, int frequency);
+    ~Node();
+public: // Member Functions
+    unordered_map<int, FrequencyNode> &getIds();
+    string &getWord();
+    void setWord(string word);
+    void insertId(int id);
+    void insertId(int id, int frequency);
+    bool exists(int id);
+    friend ostream& operator<<(ostream& out, Node& node)
     {
-        this->word = word;
-    }
-
-    Node(string& word, int id)
-    {
-        this->word = word;
-        this->ids.emplace(id, id);
-    }
-
-    unordered_map<int, int> &getIds()
-    {
-        return ids;
-    }
-
-    string &getWord()
-    {
-        return word;
-    }
-
-    void insertId(int id)
-    {
-        if(exists(id))
+        out << node.word;
+        for (auto it = node.getIds().begin(); it != node.getIds().end(); ++it)
         {
-            return;
+            out << " " << it->second;
         }
-        ids.emplace(id, id);
+        out << " !";
+        return out;
     }
-
-    bool exists(int id)
-    {
-        auto search = ids.find(id);
-        if (search != ids.end())
-            return true;
-        else
-            return false;
-    }
-
-//    friend ostream& operator<<(ostream& out, Node& node)
-//    {
-//        out << node.word;
-//        for (auto it = node.getIds().begin(); it != node.getIds().end(); ++it)
-//        {
-//            out << ", " << it->second;
-//        }
-//        return out;
-//    }
 };
 
 #endif // NODE_H
